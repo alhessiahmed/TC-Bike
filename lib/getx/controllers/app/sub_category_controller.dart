2dea41fbf3f53dedd4ccfffa1024e3b6975260c0
alhessiahmed <alhessiahmed@gmail.com>
2023-02-14 13:15:35 +0200

    fixed products displayed wrong in subcategories and added app icon and fixed app name

--- lib/getx/controllers/app/sub_category_controller.dart
+++ lib/getx/controllers/app/sub_category_controller.dart
@@ -10,32 +10,38 @@ class SubCategoryController extends GetxController {
   factory SubCategoryController() {
     return _instance ??= SubCategoryController._();
   }
-  late int categoryID;
-  late String categoryName;
+  RxInt categoryID = 0.obs;
+  RxString categoryName = ''.obs;
   final catIndex = 0.obs;
   final isLoading = false.obs;
   final isProductsLoading = false.obs;
   final subCategories = <Category>[].obs;
   final products = <Product>[].obs;
 
-  @override
-  void onInit() async {
-    isLoading(products.isEmpty);
-    await readSubCategories();
-    if (products.isEmpty && subCategories.isNotEmpty) {
-      await readSubCategoryProducts(id: subCategories[0].id);
-    }
-    isLoading(false);
-    super.onInit();
-  }
+  // @override
+  // void dispose() {
+  //   catIndex(0);
+  //   super.dispose();
+  // }
+
+  // @override
+  // void onInit() async {
+  //   isLoading(products.isEmpty);
+  //   await readSubCategories();
+  //   if (products.isEmpty && subCategories.isNotEmpty) {
+  //     await readSubCategoryProducts(id: subCategories[0].id);
+  //   }
+  //   isLoading(false);
+  //   super.onInit();
+  // }
 
   Future<void> readSubCategories() async {
     // subCategories.value = [
     //   Category(id: -1, name: 'الكل'),
     // ];
     // subCategories.addAll(await ContentApiController().readSubCategories());
-    subCategories.value =
-        await ContentApiController().readSubCategories(categoryId: categoryID);
+    subCategories.value = await ContentApiController()
+        .readSubCategories(categoryId: categoryID.value);
   }
 
   Future<void> readSubCategoryProducts({required int id}) async {
